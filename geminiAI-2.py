f60acf62d73b532df65c7f9c2d12fb906cad7c78
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2024-02-17 23:56:20 +0000

    update GeminiAI code

---
+++ geminiAI-2.py
@@ -0,0 +1,39 @@
+import google.generativeai as genai
+
+genai.configure(api_key="<YOUR API KEY>")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+#gemini-1.0-pro
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat()
+
+convo.send_message("hello! what is special about the James Webb Space Telescope? Provide answer in Vietnamese.")
+print(convo.last.text)
